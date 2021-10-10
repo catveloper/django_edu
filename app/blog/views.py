c0a1b4c17593b4ca06f08f65011ed50df60d4694
donguk im <duim91@gmail.com>
2021-10-10 22:50:40 +0900

    Add Blog view (List/Detail)

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import *
+
+from app.blog.models import Post
+
+
+class PostLV(ListView):
+    model = Post
+    template_name = 'blog/post_all.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+
+class PostDV(DetailView):
+    model = Post
+
+
+class PostAV(ArchiveIndexView):
+    model = Post
+    date_field = 'modify_dt'
