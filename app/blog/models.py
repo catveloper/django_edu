9a8c095d5b8a2cf996a9579737f8895a8fad2ea6
donguk im <duim91@gmail.com>
2021-10-11 11:34:04 +0900

    Update urlpatterns(post_list,post_detail)

--- app/blog/models.py
+++ app/blog/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blog:post:detail', args=(self.slug,))
+        return reverse('blog:post_detail', args=(self.slug,))
 
     def get_previous(self):
         return self.get_previous_by_modify_dt()
--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
+
+from app.blog import views
 
 app_name = 'blog'
 
 urlpatterns = [
+    path('', views.PostLV.as_view(), name='index'),
+    path('post/', views.PostLV.as_view(), name='post_list'),
+    re_path(r'^post/(?P<slug>[-\w]+)/$', views.PostDV.as_view(), name='post_detail'),
 ]
