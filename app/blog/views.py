eb5a30a7585020eb421c53deb2f5e5e943a91486
donguk im <duim91@gmail.com>
2021-10-11 11:33:10 +0900

    Add Blog view (ArchiveView)

--- app/blog/views.py
+++ app/blog/views.py
@@ -20,3 +20,24 @@ class PostDV(DetailView):
 class PostAV(ArchiveIndexView):
     model = Post
     date_field = 'modify_dt'
+
+
+class PostYAV(YearArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+    make_object_list = True
+
+
+class PostMAV(MonthArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostDAV(DayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
+
+
+class PostTAV(TodayArchiveView):
+    model = Post
+    date_field = 'modify_dt'
