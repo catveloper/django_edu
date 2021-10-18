0a09e89b6ec0d47cc9b46a416bf81adddaca55f9
donguk im <duim91@gmail.com>
2021-10-18 22:59:47 +0900

    메인페이지 제작

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from config.views import HomeView
+
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('bookmark/', include('app.bookmark.urls')),
     path('blog/', include('app.blog.urls')),
