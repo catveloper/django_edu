80b26ce2d6c448f2adaaf9ee7c5067d252123cfe
donguk im <duim91@gmail.com>
2021-10-19 23:56:37 +0900

    tag로 묶어 보기 , tag 리스트 추가

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path('archive/<int:year>/<int:month>/', views.PostMAV.as_view(), name='post_month_archive'),
     path('archive/<int:year>/<int:month>/<int:day>/', views.PostDAV.as_view(), name='post_day_archive'),
     path('archive/today/', views.PostTAV.as_view(), name='post_today_archive'),
+    path('archive/tag/', views.TagCloudTV.as_view(), name='tag_cloud'),
+    path('archive/tag/<str:tag>/', views.TaggedObjectLV.as_view(), name='tagged_object_list'),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -50,3 +50,20 @@ class PostTAV(TodayArchiveView):
     template_name = 'blog/post_archive_day.html'
     date_field = 'modify_dt'
     month_format = '%m'
+
+
+class TagCloudTV(TemplateView):
+    template_name = 'tag/tag_cloud.html'
+
+
+class TaggedObjectLV(ListView):
+    model = Post
+    template_name = 'tag/tagged_post_list.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__name=self.kwargs.get('tag'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tagname'] = self.kwargs['tag']
+        return context
